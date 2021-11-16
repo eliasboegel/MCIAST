a63a6c5df697122691e749fdfe84fdbfd92400db
elias <elias.boegel@yahoo.de>
2021-11-16 14:16:28 +0100

    Initial dev branch commit

--- mciast.py
+++ mciast.py
@@ -1,8 +1,22 @@
-from ctypes import c_void_p, c_double, c_int, cdll
+import platform, os
+import mciast_iast
 
-mciast_lib = cdll.LoadLibrary("./mciast.so")
+import numpy as np
+np.set_printoptions(edgeitems=30, linewidth=1000)
 
+class Mixture:
+    n_components = None
+    __available_models = ['sslangmuir']
+    partial_pressures = None
+    isotherm_model = None
+    isotherm_data = None
 
-#spreading_pressure_langmuir = lib.spreading_pressure_langmuir
-#spreading_pressure_langmuir.restype = c_double
+    def __init__(self, partial_pressures, isotherm_model, isotherm_data):
+        self.n_components = partial_pressures.shape[0]
+        self.partial_pressures = partial_pressures
+        self.isotherm_model = isotherm_model
+        self.isotherm_data = isotherm_data
 
+def solve_iast(Mixture):
+    return mciast_iast.__iast_solve(Mixture)
+    
