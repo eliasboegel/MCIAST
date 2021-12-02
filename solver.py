Finish up tests for dp_dt computation, fix errors in matrix generation

solver.py
@@ -93,12 +93,13 @@ class Solver:
         self.g_matrix = (1 / self.params.dz) ** 2 * self.g_matrix
 
         self.l_matrix = np.diag(np.full(self.params.n_points - 1, 1), -1) + np.diag(
-            np.full(self.params.n_points - 1, 1), 1) + np.diag(np.full(self.params.n_points, -1), 0)
+            np.full(self.params.n_points - 1, 1), 1) + np.diag(np.full(self.params.n_points, -2), 0)
         self.l_matrix[self.l_matrix.shape[0] - 1][self.l_matrix.shape[1] - 2] = 2
         self.l_matrix[self.l_matrix.shape[0] - 1][self.l_matrix.shape[1] - 1] = -2
+        self.l_matrix = (1 / self.params.dz) ** 2 * self.l_matrix
 
         self.d_matrix = np.zeros((self.params.n_points, self.params.n_components))
-        first_row = (self.params.p_partial_in / self.R * self.params.temp) * (
+        first_row = (self.params.p_partial_in / (self.R * self.params.temp)) * (
                 (self.params.v_in / (2 * self.params.dz)) + (self.params.disp / (self.params.dz ** 2)))
         self.d_matrix[0] = first_row  # idk if that works
 
@@ -130,10 +131,20 @@ class Solver:
         :param ro_p: Density of the adsorbent (?)
         :return: Matrix containing the time derivatives of partial pressures of each component at each grid point.
         """
+        # This can be removed if we assume that a matrix in a correct form is passed
+        velocities = velocities.reshape((-1, 1))
+
         m_matrix = np.multiply(velocities, p_partial)
+        print(m_matrix)
         dp_dt = -np.dot(self.g_matrix, m_matrix) + self.params.disp * np.dot(self.l_matrix, p_partial) - \
             self.params.temp * self.R * ((1 - self.params.void_frac) / self.params.void_frac) * self.params.rho_p *\
-            np.transpose(self.params.kl) * (q_eq - q_ads) + self.d_matrix
+            self.params.kl * (q_eq - q_ads) + self.d_matrix
+        print("start")
+        print(-np.dot(self.g_matrix, m_matrix))
+        print(self.params.disp * np.dot(self.l_matrix, p_partial))
+        print(self.params.temp * self.R * ((1 - self.params.void_frac) / self.params.void_frac) * self.params.rho_p *\
+            self.params.kl * (q_eq - q_ads))
+        print(self.d_matrix)
         return dp_dt
 
     def verify_pressures(self, p_partial):

test_solver.py
@@ -33,27 +33,47 @@ class TestSolver(unittest.TestCase):
         np.testing.assert_array_equal(params.p_total, np.asarray([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]))
 
     def test_initialize_solver(self):
-        n_points = 11
+        n_points = 3
         n_components = 2
-        params = SysParams(t_end=8, dt=0.001, y_in=np.asarray([0.2, 0.8]), n_points=n_points, p_in=5.0, p_out=5.0, temp=313,
-                           c_len=4, u_in=2,
-                           void_frac=0.6, disp=16, kl=6, p_he=12, rho_p=2)
+        params = SysParams(t_end=8, dt=0.001, y_in=np.asarray([0.2, 0.8]), n_points=3, p_in=5.0, p_out=5.0, temp=313,
+                           c_len=1, u_in=1,
+                           void_frac=0.6, disp=[1, 1], kl=[1, 1], p_he=2, rho_p=2)
         solver = Solver(params)
         print("Matrix G: ", solver.g_matrix)
         print("Matrix L: ", solver.l_matrix)
         print("Matrix D: ", solver.d_matrix)
         print("Vector B: ", solver.b_vector)
         self.assertEqual(solver.g_matrix.shape, (n_points, n_points))
+        np.testing.assert_allclose(solver.g_matrix, [[0., 4., 0.],
+                                                     [-4., 0., 4.],
+                                                     [4., -16., 12.]], 1e-5)
         self.assertEqual(solver.l_matrix.shape, (n_points, n_points))
+        np.testing.assert_allclose(solver.l_matrix, [[-8., 4., 0.],
+                                                     [4., -8., 4.],
+                                                     [0., 8., -8.]], 1e-5)
         self.assertEqual(solver.d_matrix.shape, (n_points, n_components))
-        self.assertEqual(solver.b_vector.shape, (n_points, ))
+        np.testing.assert_allclose(solver.d_matrix, [[0.00192139, 0.00768556],
+                                                     [0., 0.],
+                                                     [0., 0.]], 1e-5)
+        self.assertEqual(solver.b_vector.shape, (n_points,))
 
-    def test_caclulate_dp_dt(self):
+    def test_caclulate_dp_dt1(self):
+        """
+        Case in which kl and disp are identical for each component and equal to 1.
+        """
         params = SysParams(t_end=8, dt=0.001, y_in=np.asarray([0.2, 0.8]), n_points=3, p_in=5.0, p_out=5.0, temp=313,
-                           c_len=4, u_in=2,
-                           void_frac=0.6, disp=16, kl=6, p_he=12, rho_p=2)
+                           c_len=1, u_in=1,
+                           void_frac=0.6, disp=[1, 1], kl=[1, 1], p_he=2, rho_p=2)
         solver = Solver(params)
 
-        dp_dt = solver.calculate_dp_dt([1, 2, 1], np.asarray([[0.3, 0.7], [0.35, 0.65]]), np.asarray([[1, 2], [3, 4], [2, 2]]),
+        dp_dt = solver.calculate_dp_dt(np.asarray([1, 2, 1]), np.asarray([[0.3, 0.7], [0.35, 0.65], [0.4, 0.6]]),
+                                       np.asarray([[1, 2], [3, 4], [2, 2]]),
                                        np.asarray([[0.5, 1.5], [2, 3], [1, 1]]))
+        # Use this to test in Wolfram Alpha
+        # -(Divide[1,0.25])*{{0,1,0},{-1,0,1},{1,-4,3}}{{0.3,0.7},{0.7,1.3},{0.4,0.6}}+(Divide[1,0.25])*{{-2,1,0},
+        # {1,-2,1},{0,2,-2}}{{0.3,0.7},{0.35,0.65},{0.4,0.6}}- Divide[\(40)1-0.6\(41),0.6]*2*8.314*313
+        # {{0.5,0.5},{1,1},{1,1}}+{{188.2367,752.946},{0,0},{0,0}}
+        np.testing.assert_allclose(dp_dt, np.asarray([[-1738.652745, -1743.046981],
+                                                      [-3470.109333, -3469.309333],
+                                                      [-3464.909333, -3458.509333]]), 1e-1)
         print("dp_dt: ", dp_dt)