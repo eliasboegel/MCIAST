7413e47e42983dc65fbf6b37f94ffbafec93608d
4ndries <50869735+4ndries@users.noreply.github.com>
2021-12-07 17:28:37 +0100

    Update method_of_lines_main.py

--- method_of_lines_main.py
+++ method_of_lines_main.py
@@ -13,19 +13,39 @@ from math import exp
 plot = True
 fast_plot = True
 
+
 # Grid size and snapshots
-m = 400
+m = 101
 #number of the grid points
-snaps= 400#number of time steps forward
+snaps= 100#number of time steps forward
 
 
 # RHS of ODE system from semi-discretization
 def deriv(u,t):
-    return -cidx*(np.roll(u,1)-np.roll(u,-1))*0.5
+
+    
+    #forward difference
+        #Fdu = -cidx*(-3*np.roll(u,0) + 4*np.roll(u,1) - np.roll(u,2)) * (1/2)
+    Fdu = -cidx*(np.roll(u[0:2],0) - np.roll(u[0:2],1))
+
+    #backward difference
+    Bdu = cidx*(-3*np.roll(u[m-1:m+1],0) + 4*np.roll(u[m-1:m+1],-1) - np.roll(u[m-1:m+1],-2)) * (1/2)
+    
+    
+    #central difference 
+    Zdu = -cidx*(-np.roll(u,2)+8*np.roll(u,1)-8*np.roll(u,-1)+np.roll(u,2))*(1/12) #O^4 error
+    Xdu = -cidx*(np.roll(u[2:m-1],1) - np.roll(u[2:m-1],-1))*(0.5) #O^2 error
+   
+    du = np.concatenate((Fdu,Xdu,Bdu))
+    #print(du-Zdu)
+    
+    
+
+    return Zdu
 
 # PDE-related constants
 c=0.1
-dx=1.0/m
+dx=1/m
 cidx=c/dx
 dt=0.1
 
@@ -33,10 +53,10 @@ dt=0.1
 uinit=np.empty((m))
 for i in range(m):
     x=dx*i
-    uinit[i]=exp(-20*(x-0.5)**2)
+    uinit[i]= exp(-20*(x-0.5)**2) # 1/(x-0.5) # exp(-20*(x-0.5)**2)
 
 # Define the times for saving snapshots
-time=np.linspace(0,dt*snaps,snaps+1)
+time =np.linspace(0,dt*snaps,snaps+1)
 
 # Integrate the semi-discretized PDE
 u=odeint(deriv,uinit,time);
