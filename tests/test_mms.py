08c5ccbd4d46c75f8dd694ed9975a394f7ab1a78
janstruzinski <31801358+janstruzinski@users.noreply.github.com>
2021-12-30 17:39:34 +0100

    Add unit tests for MMS

--- tests/test_mms.py
+++ tests/test_mms.py
@@ -5,40 +5,151 @@ import numpy as np
 from src.linearized_system import LinearizedSystem
 from src.solver import Solver
 from src.system_parameters import SysParams
+from src.mms import MMS
 
 
 class TestMMS(unittest.TestCase):
-    params = SysParams()
-    params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.2, 0.8]), n_points=11, p_in=5.0, p_out=5.0, temp=313,
-                       c_len=4, u_in=2, void_frac=0.6, disp=[16, 8], kl=[6, 8], rho_p=2, dispersion_helium=8)
-
     def test_pt_calculation(self):
-        ...
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
+
+        np.testing.assert_array_equal(params.p_total, np.array([0.875, 0.75, 0.625, 0.5]))
 
     def test_pi_calculation(self):
-        ...
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
+
+        np.testing.assert_allclose(mms.pi_matrix, np.array([[1.241250107, -0.8037501065, 1.241250107, -0.8037501065],
+                                                            [1.799444199, -1.424444199, 1.799444199, -1.424444199],
+                                                            [1.719946207, -1.407446207, 1.719946207, -1.407446207],
+                                                            [1.125, -0.875, 1.125, -0.875]]), atol=1e-9)
 
     def test_dpi_dt_calculation(self):
-        ...
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
 
-    def test_dpi_dx_calculation(self):
-        ...
+        np.testing.assert_allclose(mms.dpi_dt_matrix, 1e-6 *
+                                   np.array([[-639.8166742, 639.8166742, -639.8166742, 639.8166742],
+                                            [-904.837418, 904.837418, -904.837418, 904.837418],
+                                            [-639.8166742, 639.8166742, -639.8166742, 639.8166742],
+                                            [0.0, 0.0, 0.0, 0.0]]), atol=1e-9)
 
-    def test_d2pi_dx2_calculation(self):
-        ...
+    def test_dpi_dz_calculation(self):
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
 
-    def test_v_calculation(self):
-        ...
+        np.testing.assert_allclose(mms.dpi_dz_matrix,
+                                   np.array([[3.336269939, -3.586269939, 3.336269939, -3.586269939],
+                                            [0.9857207345, -1.235720735, 0.9857207345, -1.235720735],
+                                            [-1.533926633, 1.283925633, -1.533926633, 1.283925633],
+                                            [-2.967630585, 2.717630585, -2.967630585, 2.717630585]]), atol=1e-9)
 
-    def test_dv_dx_calculation(self):
-        ...
+    def test_d2pi_dz2_calculation(self):
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
 
-    def test_dpv_dx_calculation(self):
-        ...
+        np.testing.assert_allclose(mms.d2pi_dz2_matrix,
+                                   np.array([[-7.258970985, 7.258970985, -7.258970985, 7.258970985],
+                                             [-10.67510341, 10.67510341, -10.67510341, 10.67510341],
+                                             [-8.594318838, 8.594318838, -8.594318838, 8.594318838],
+                                             [-2.4674011, 2.4674011, -2.4674011, 2.4674011]]), atol=1e-9)
+
+    def test_nu_calculation(self):
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
+
+        np.testing.assert_allclose(mms.nu, np.array([0.3562395748, -0.2583908086, 0.08564152472, 0.5]), atol=1e-9)
+
+    def test_dnu_dz_calculation(self):
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
+
+        np.testing.assert_allclose(mms.dnu_dz, np.array([2.117017297, -0.5553603673, -3.14318945, 0.0]), atol=1e-9)
+
+    def test_dnupi_dz_calculation(self):
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
+
+        np.testing.assert_allclose(mms.dnupi_dz_matrix,
+                                   np.array([[3.81625933, -2.979124156, 3.81625933, -2.979124156],
+                                             [-1.254041169, 1.110378733, -1.254041169, 1.110378733],
+                                             [-5.537484588, 4.533827418, -5.537484588, 4.533827418],
+                                             [-1.483815283, 1.358815293, -1.483815283, 1.358815293]]), atol=1e-9)
 
     def test_S_nu_calculation(self):
-        ...
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
+
+        np.testing.assert_allclose(mms.S_nu,
+                                   1e6 * np.array([17.54923263, 35.09846091, 52.64768978, 70.19692214]), atol=1e-9)
 
     def test_S_pi_calculation(self):
-        ...
+        params = SysParams()
+        params.init_params(t_end=8, dt=0.001, y_in=np.asarray([0.25, 0.25, 0.25, 0.25]), n_points=5, p_in=1.0,
+                           p_out=0.5,
+                           temp=288, c_len=1, u_in=1, void_frac=0.1, disp=[0.001, 0.001, 0.001, 0.001], kl=[1, 1, 1, 1],
+                           rho_p=1000, mms=True, ms_pt_distribution="linear", mms_mode="transient",
+                           mms_convergence_factor=1000)
+        mms = MMS(params)
+        mms.update_source_functions(100)
+
+        np.testing.assert_allclose(mms.S_pi,
+                                   1e6 * np.array([[4.387311472, 4.387304663, 4.387311472, 4.387304663],
+                                                  [8.774614054, 8.774616399, 8.774614054, 8.774616399],
+                                                  [13.16191742, 13.16192747, 13.16191742, 13.16192747],
+                                                  [17.54922912, 17.54923195, 17.54922912, 17.54923195]]), atol=1e-9)
 
