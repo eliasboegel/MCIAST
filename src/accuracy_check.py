Fix lack of boundary for laplacian operator on partial pressures for velocity calculation

src/accuracy_check.py
@@ -5,17 +5,19 @@ from src.system_parameters import SysParams
 
 
 class OrderOfAccuracy:
-    def __init__(self, which, n, dt):
+    def __init__(self, which, n, dt, r):
         # Initialize parameters
         self.n = n
         self.dt = dt
         self.type = which
+        self.r = r
         if self.type != "Space" and self.type != "Time":
             raise Warning("which of OoM must be either Space or Time")
 
     def analysis(self):
         # Create a list of discretizations that will be used to estimate convergence.
-        discretization_list = [(self.dt, self.n), (self.dt / 2, 2 * self.n), (self.dt / 3, 3 * self.n)]
+        discretization_list = [(self.dt, self.n), (self.dt / self.r, self.r * self.n),
+                               (self.dt / (self.r**2), self.r**2 * self.n)]
         # Create a list to store errors between calculated and manufactured solutions for discretizations above
         error_list = []
         # Create params class that will store steady state (final solution). It is used for both space and time OoA
@@ -24,11 +26,11 @@ class OrderOfAccuracy:
         ss_params = SysParams()
         for (dt, nodes) in discretization_list:
             # Set ss_params with tighter discretization for each run
-            ss_params.init_params(t_end=10000, dt=dt, y_in=np.asarray([0.5, 0.5]), n_points=nodes, p_in=2e5,
-                                  temp=298, c_len=1, u_in=1, void_frac=0.995, disp=[0.004, 0.004], kl=[4.35, 1.47],
-                                  rho_p=1000, p_out=2e5, time_stepping="BE", dimensionless=True,
-                                  dispersion_helium=0.004, mms=True, ms_pt_distribution="linear",
-                                  mms_mode="steady", mms_convergence_factor=1000)
+            ss_params.init_params(t_end=10000, dt=dt, y_in=np.asarray([0.4, 0.3, 0.3]), n_points=nodes, p_in=2e5,
+                                  temp=298, c_len=1, u_in=1, void_frac=0.995, disp=[0.004, 0.004, 0.004],
+                                  kl=[4.35, 1.4, 1.4], rho_p=1000, p_out=1.99e5, time_stepping="BE", dimensionless=True,
+                                  mms=True,  ms_pt_distribution="linear", mms_mode="steady",
+                                  mms_convergence_factor=1000)
             ss_solver = Solver(ss_params)
             error_matrix = None
             if self.type == "Space":
@@ -50,11 +52,11 @@ class OrderOfAccuracy:
             elif self.type == "Time":
                 # Create time parameters and solver
                 t_params = SysParams()
-                t_params.init_params(t_end=10000, dt=dt, y_in=np.asarray([0.5, 0.5]), n_points=nodes, p_in=2e5,
-                                     temp=298, c_len=1, u_in=1, void_frac=0.995, disp=[0.004, 0.004], kl=[4.35, 1.47],
-                                     rho_p=1000, p_out=2e5, time_stepping="BE", dimensionless=True,
-                                     dispersion_helium=0.004, mms=True, ms_pt_distribution="linear",
-                                     mms_mode="transient", mms_convergence_factor=1000)
+                t_params.init_params(t_end=10000, dt=dt, y_in=np.asarray([0.4, 0.3, 0.3]), n_points=nodes, p_in=2e5,
+                                     temp=298, c_len=1, u_in=1, void_frac=0.995, disp=[0.004, 0.004, 0.004],
+                                     kl=[4.35, 1.4, 1.4], rho_p=1000, p_out=2e5, time_stepping="BE", dimensionless=True,
+                                     mms=True,  ms_pt_distribution="linear", mms_mode="transient",
+                                     mms_convergence_factor=1000)
                 t_solver = Solver(t_params)
 
                 # Get the calculated solution
@@ -68,6 +70,7 @@ class OrderOfAccuracy:
                 error_matrix = p_i_calc - p_i_manufactured
 
             # Use RMS error over nodes and then over components (to make the largest error have the biggest weight)
+            print(error_matrix)
             error_norm_i = np.sqrt(np.mean(error_matrix ** 2, axis=0))
             error_norm = np.sqrt(np.mean(error_norm_i ** 2))
             error_list.append(error_norm)
@@ -78,5 +81,5 @@ class OrderOfAccuracy:
         return order_of_accuracy, discretization_list
 
 
-ooa = OrderOfAccuracy(which="Space", n=100, dt=0.001)
+ooa = OrderOfAccuracy(which="Space", n=30, dt=0.001, r=2)
 print(ooa.analysis()[0])

src/solver.py
@@ -32,15 +32,15 @@ class Solver:
         # print(f"f_matrix: {self.f_matrix}")
         # Calculate the terms in the equation
         ldf = np.multiply(self.params.kl_matrix, q_eq - q_ads)
-        lp = np.multiply(self.params.disp_matrix / (self.params.R * self.params.temp),
+        lp = np.multiply(self.params.disp_matrix,
                          self.params.l_matrix.dot(p_partial))
-        component_sums = np.sum(self.params.void_frac_term * ldf - lp, axis=1)
+        component_sums = np.sum(self.params.R * self.params.temp * self.params.void_frac_term * ldf - lp, axis=1)
         # Add up the terms in the equations for RHS
         if self.params.mms is True:
-            rhs = -self.params.R * self.params.temp * component_sums / self.params.p_total - self.params.b_v_vector + \
+            rhs = -(component_sums + self.params.e_vector) / self.params.p_total - self.params.b_v_vector + \
                   self.MMS.S_nu
         else:
-            rhs = -self.params.R * self.params.temp * component_sums / self.params.p_total - self.params.b_v_vector
+            rhs = -(component_sums + self.params.e_vector) / self.params.p_total - self.params.b_v_vector
         # Create LHS of the equation and solve
         lhs = self.params.g_matrix + self.params.f_matrix
         velocities = sp.linalg.spsolve(lhs, rhs)

src/system_parameters.py
@@ -45,6 +45,7 @@ class SysParams:
         self.l_matrix = 0
         self.d_matrix = 0
         self.b_v_vector = 0
+        self.e_vector = 0
         self.xi = 0
 
     def init_params(self, y_in, n_points, p_in, p_out, temp, c_len, u_in, void_frac, disp, kl, rho_p,
@@ -166,9 +167,9 @@ class SysParams:
             self.dp_dz = (p_out - p_in) / self.c_len
 
         # Parameters for outlet boundary condition
-        if self.p_in == self.p_out:
+        if self.p_in == self.p_out or self.ms_pt_distribution == "constant":
             self.outlet_boundary_type = "Neumann"
-        elif self.p_in != self.p_out:
+        elif self.p_in != self.p_out or self.ms_pt_distribution == "linear":
             self.outlet_boundary_type = "Numerical"
         elif self.outlet_boundary_type != "Neumann" and self.outlet_boundary_type != "Numerical":
             raise Warning("Outlet boundary condition needs to be either Neumann or Numerical")
@@ -238,6 +239,10 @@ class SysParams:
                 (self.v_in / (2 * self.dz)) + (self.disp / (self.dz ** 2)))
         self.d_matrix[0] = first_row
         # self.d_matrix = sp.csr_matrix(self.d_matrix)
+
+        # Create matrix for inlet boundary condition for laplacian operator on partial pressures
+        self.e_vector = np.zeros(self.n_points - 1)
+        self.e_vector[0] = - np.sum(self.p_partial_in * self.disp)
 
         # Create vector for velocity equation for storing inlet boundary condition
         self.b_v_vector = np.zeros(self.n_points - 1)